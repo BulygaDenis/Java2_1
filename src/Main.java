1766613983b55d761a124b22b01fe85760359304
Neposeda644 <denis_bulyga@mail.ru>
2021-01-09 20:58:30 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10000;
+        int onemile = 20;
+        long bonus = amount / onemile;
+        System.out.println(bonus);
+    }
+}
+
